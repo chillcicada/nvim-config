d46be029be7064887760236e9385495e267805e7
chillcicada <2210227279@qq.com>
2025-04-12 11:17:25 +0800

    chore: update neotree config

--- lua/plugins/edit/neotree.lua
+++ lua/plugins/edit/neotree.lua
@@ -4,6 +4,13 @@ return {
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".DS_Store",
+          ".git",
+          "__pycache__",
+          "node_modules",
+        },
       },
     },
   },
